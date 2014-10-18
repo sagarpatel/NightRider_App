5e4e2dc72fe8f4bd61b7a803a3717468eeb52601
Sagar Patel <17.sagar.patel@gmail.com>
2014-10-18 18:14:53 +0900

    Added basic assets and script, it basically works, just need to package into app and add art

---
+++ NightRiderAppUnityFolder/NightRiderAppUnityProject/Assets/Scripts/Audiodirector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audiodirector : MonoBehaviour 
+{
+
+	public AudioSource audioSource;
+
+	void Start () 
+	{
+		audioSource.Play();
+		audioSource.loop = true;
+	}
+
+
+
+	void Update () 
+	{
+
+	
+	}
+}
