9caf839401c1cc10730a8f6158e1e8bc15cadafd
Sagar Patel <17.sagar.patel@gmail.com>
2014-10-18 18:38:19 +0900

    Added rotating cube and audio color

---
+++ NightRiderAppUnityFolder/NightRiderAppUnityProject/Assets/Scripts/ApplyAudioColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyAudioColor : MonoBehaviour 
+{
+	AudioDirectorScript audioDirector;
+	Material objectMaterial;
+
+	void Start () 
+	{
+		audioDirector = FindObjectOfType<AudioDirectorScript>().GetComponent<AudioDirectorScript>();
+		objectMaterial = renderer.material;
+	}
+
+
+	void Update () 
+	{
+		objectMaterial.color = audioDirector.calculatedRGB;
+	}
+
+
+}
---
+++ NightRiderAppUnityFolder/NightRiderAppUnityProject/Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour 
+{
+
+	public float rotationSpeed = 10.0f;
+
+
+	void Start () 
+	{
+	
+	}
+
+
+	void Update () 
+	{
+		float rot = rotationSpeed * Time.deltaTime;
+		transform.Rotate(new Vector3(rot, 0, rot),Space.Self);
+	}
+}
